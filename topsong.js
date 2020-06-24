096bea68aaf703c64a725e76f20f32c0b4ef013f
Haley SuYoun Jeon <tndus0604@hotmail.com>
2020-06-24 12:52:51 -0400

    created .env and .gitignore

--- topsong.js
+++ topsong.js
@@ -5,8 +5,8 @@ const cTable = require('console.table');
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
-    user: "root",
-    password: "hyomi1338",
+    user: process.env.password,
+    password: process.env.password,
     database: "top_songsDB"
   });
 
